3da5ec61ecfb09cb9a04933e50a969dd3c5d4b47
Laily97 <79189049+Laily97@users.noreply.github.com>
2022-04-04 10:07:32 +0800

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import FilterComponent from './components/FilterComponent';
+import BusTypeComponent from './components/BusTypeComponent';
 
 function App() {
   return (
     <div className="App">
       <FilterComponent />
+      <div style={{display: 'flex';}}>
+          <BusTypeComponent />
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/BusTypeComponent.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class BusTypeComponent extends React.Component{
+
+    render(){
+        return(
+        <>
+            <div style={{border: '1px solid'; display: 'flex'}}>
+                <div>
+                    <label>Bus stop code:</label><br />
+                    <select className="form-control">
+                        <option>Select </option>
+                    </select>
+                </div>
+
+                <div>
+                    <label>Bus number:</label><br />
+                    <select className="form-control">
+                        <option>Select </option>
+                    </select>
+                </div>
+            </div>
+        </>
+        )
+    }
+}
+
+export default BusTypeComponent
